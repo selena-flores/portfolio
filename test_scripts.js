ed5d30915a932663f8a616a8937346ba85edfbe2
kudacat <kudacat.io@gmail.com>
2025-01-09 11:56:09 -0800

    intro back

--- test_scripts.js
+++ test_scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   // var dataText = ['I find myself enjoying work with the optimists, do you? Email me at netusersel@gmail.com to grab a cup of coffee anywhere in Portland or virtually. Let’s build something.'];
   var dataText = [
-    // "i'm selena",
-    // "a people & technology specialist",
-    // "let's build something together.",
+    "i'm selena",
+    "a people & technology specialist",
+    "let's build something together.",
   ];
   function typeWriter(text, i, fnCallback) {
     if (i < text.length) {
